7fcbac0d8a9b4e4498da30d82dac59a9987cf3f4
MWFNexient <mfender@nexient.com>
2015-08-25 11:26:13 -0400

    JDK hash to GNU Crypto

--- java/src/main/MDhash.java
+++ java/src/main/MDhash.java
@@ -7,10 +7,6 @@ import javax.xml.bind.DatatypeConverter;
 import gnu.crypto.hash.HashFactory;
 import gnu.crypto.hash.IMessageDigest;
 
-/*
- * Using jdk 1.8.0_51, 7 MessageDigests are provided. New ones should be
- * added here with a way to reference them and create them.
- */
 enum MDhash implements Function<String, String> {
 	HAVAL, MD5, RIPEMD128, RIPEMD160, SHA256("SHA-256"), SHA384("SHA-384"), SHA512("SHA-512"), TIGER, WHIRLPOOL;
 	MDhash() {
@@ -23,6 +19,7 @@ enum MDhash implements Function<String, String> {
 
 	private IMessageDigest messageDigest;
 
+	@Override
 	public String apply(final String incoming) {
 		this.messageDigest.reset();
 		final byte[] bytes = incoming.getBytes();
